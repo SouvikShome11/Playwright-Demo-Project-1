ff095c6952e854fb7b19a4609cffe86ef2fcd599
Souvik Shome <souvikshome11@gmail.com>
2026-08-09 07:48:38 +0530

    config change

--- playwright.config.ts
+++ playwright.config.ts
@@ -50,21 +50,5 @@ export default defineConfig({
       },
       dependencies: ["setup"],
     },
-    {
-      name: "firefox",
-      use: {
-        ...devices["Desktop Firefox"],
-        storageState: "playwright/.auth/user1.json",
-      },
-      dependencies: ["setup"],
-    },
-    {
-      name: "webkit",
-      use: {
-        ...devices["Desktop Safari"],
-        storageState: "playwright/.auth/user1.json",
-      },
-      dependencies: ["setup"],
-    },
   ],
 });
